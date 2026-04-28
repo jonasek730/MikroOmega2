5c8c47ab410afa633716608552811f29735d8fb1
majerle <majerle@spsejecna.cz>
2026-04-28 08:44:28 +0200

    initial comit

---
+++ src/Pole.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Pole {
+    Wall [][] pole = new Wall[50][50];
+
+    public Wall[][] getPole() {
+        return pole;
+    }
+
+    public void setPole(Wall[][] pole) {
+        this.pole = pole;
+    }
+    public void createMaze(){
+        for (int i = 0; i < pole.length; i++) {
+            for (int j = 0; j < pole[i].length; j++) {
+            pole[i][j].setWall(true);
+            }
+
+        }
+    }
+
+    public boolean printMaze(){
+        for (int i = 0; i < pole.length; i++) {
+            for (int j = 0; j < pole[i].length; j++) {
+                if(pole[i][j].isWall()){
+                    System.out.println("#");
+                }else{
+                    System.out.println(",");
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/Wall.java
@@ -0,0 +1,21 @@
+public class Wall {
+    private boolean isWall;
+    private boolean isKey;
+    private boolean isEnd;
+
+    public boolean isWall() {
+        return isWall;
+    }
+
+    public boolean isKey() {
+        return isKey;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+    public void setWall(boolean wall) {
+        isWall = wall;
+    }
+}
